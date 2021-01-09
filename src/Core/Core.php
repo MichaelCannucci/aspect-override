76a6cea5ff9fd48cb0e8e4eb5115e30213e0ed94
Michael Cannucci <michaelcannucci@gmail.com>
2021-01-09 03:56:22 -0500

    move configuration to it's own class

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -4,19 +4,14 @@ namespace AspectOverride\Core;
 
 use AspectOverride\Loader\AutoloaderHijacker;
 use AspectOverride\Loader\AutoloaderWrapper;
+use AspectOverride\Util\Configuration;
 use Composer\Autoload\ClassLoader;
 use RuntimeException;
 
 class Core
 {
-  /** @var string[] */
-  protected $directories  = [];
-  /** @var bool */
-  protected $hasBeenInitialized = false;
-  /** @var string */
-  protected $temporaryFilesDir = '/tmp/';
-  /** @var bool */
-  protected $useCache = false;
+  /** @var Configuration */
+  protected $config;
   /** @var ?self */
   protected static $instance;
 
@@ -28,52 +23,29 @@ class Core
     }
     return self::$instance;
   }
-  /** @param array{directories:string[],temporaryFilesDir:string,useCache:bool} $options */
+  /**
+   * @param array{
+   *  directories: string[],
+   *  temporaryFilesDir: string,
+   *  disableCaching: bool
+   * } $options 
+   */
   public function init(array $options): void
   {
-    if($this->hasBeenInitialized) {
-      return;
-    }
-    if(isset($options['temporaryFilesDir'])) {
-      $tmp = $options['temporaryFilesDir'];
-      // Ensure that the temporary directory specified always has an ending slash
-      if($tmp[strlen($tmp)] !== '/') {
-        $tmp .= '/';
-      }
-      $this->temporaryFilesDir = $tmp;
-    }
-    if(!isset($options['directories'])) {
-      throw new RuntimeException("Expecting directories to be listed!");
-    }
-    $this->useCache = $options['useCache'] ?? false;
-    $this->directories = $this->normalizeDirectories($options['directories']);
+    $this->config = new Configuration(...$options);
     AutoloaderHijacker::hijack(ClassLoader::class, new AutoloaderWrapper());
-    $this->hasBeenInitialized = true;
   }
   public function getTemporaryDirectory(): string
   {
-    return $this->temporaryFilesDir;
+    return $this->config->getTemporaryFilesDirectory();
   }
   /** @return string[] */
   public function getDirectories(): array
   {
-    return $this->directories;
+    return $this->config->getDirectories();
   }
   public function shouldUseCache(): bool
   {
-    return $this->useCache;
-  }
-  /** 
-  * @param string[] $directories 
-  * @return string[]
-  */
-  protected function normalizeDirectories(array $directories): array
-  {
-    return array_filter(array_map(
-      function (string $directory) {
-        return realpath($directory);
-      },
-      $directories
-    ));
+    return $this->config->getUseCache();
   }
 }
---
+++ src/Util/Configuration.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AspectOverride\Util;
+
+class Configuration
+{
+  /** @var string[] */
+  protected $directories;
+  /** @var string */
+  protected $tempFilesDir;
+  /** @var bool */
+  protected $disableCaching;
+
+  /** @param string[] $directories */
+  public function __construct(
+    array $directories = [], 
+    string $tempFilesDir = '/tmp/aspect-override/', 
+    bool $disableCaching = false
+  ) {
+    $this->directories    = $this->processFolders($directories);
+    $this->tempFilesDir   = $this->processTemporary($tempFilesDir);
+    $this->disableCaching = $disableCaching;
+  }
+  /** 
+   * @param string[] $directories 
+   * @return string[]
+   */
+  protected function processFolders(array $directories): array
+  {
+    return array_filter(
+      array_map(function (string $directory) {return realpath($directory);}, $directories)
+    );
+  }
+  protected function processTemporary(string $dir): string
+  {
+    // Ensure that the temporary directory specified always has an ending slash
+    if ($dir[strlen($dir) - 1] !== '/') {
+      $dir .= '/';
+    }
+    if (!is_dir($dir)) {
+      mkdir($dir, 0777, true);
+    }
+    return $dir;
+  }
+  public function getDirectories(): array
+  {
+    return $this->directories;
+  }
+  public function getTemporaryFilesDirectory(): string
+  {
+    return $this->tempFilesDir;
+  }
+  public function getUseCache(): bool
+  {
+    return $this->disableCaching;
+  }
+}
