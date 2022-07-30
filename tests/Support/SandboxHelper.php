44dede247ea815c718e5a8950a9463714391646f
Michael Cannucci <michaelcannucci@gmail.com>
2022-07-29 23:55:32 -0400

    Remove code copied from opis/closure fix invalid code being copied from closure from not removing last bracket

--- tests/Support/SandboxHelper.php
+++ tests/Support/SandboxHelper.php
@@ -25,18 +25,32 @@ class SandboxHelper
     }
     public static function getCode(\Closure $closure, bool $stripNamespaces = false): string {
         try {
-            $code = (new ReflectionClosure($closure))->getCode();
-        } catch (\ReflectionException $e) {
-            throw new \RuntimeException("Failure during sandbox setup: " . $e->getMessage());
-        }
-        // Replace wrapping closure code
-        $code = preg_replace('/static function\(.*\).+{/', '', $code);
-        // last character should always be the closing brace of a closure
-        $code = substr($code, 0, -1);
-        if($stripNamespaces) {
-            $code = preg_replace('/\\\\.+\\\\/m', '', $code);
-        }
-        return self::tempFile("<?php" . PHP_EOL . $code);
+            $fn = new \ReflectionFunction($closure);
+            $start = $fn->getStartLine();
+            $end = $fn->getEndLine();
+            $file = file_get_contents($fn->getFileName());
+            // copying namespaces from the file is hacky but it works for now :(
+            preg_match_all('/use.+/', $file, $matches);
+            $namespaces = array_map(function ($m) {return $m[0];}, $matches);
+            $code = "";
+            $lineNum = 0;
+            for($line = strtok($file, "\n"), 0; $line !== false; $line = strtok("\n")) {
+                $lineNum++;
+                if($lineNum === $start) {
+                    $line = preg_replace('/static function\(.*\).+{/', '', $line);
+                }
+                if($lineNum === $end) {
+                    $line = preg_replace('/}?,?/', '', $line);
+                }
+                if($lineNum >= $start && $lineNum <= $end) {
+                    $code .= $line . "\n";
+                }
+            }
+            if($stripNamespaces) {
+                $code = preg_replace('/\\\\.+\\\\/m', '', $code);
+            }
+            return self::tempFile("<?php" . PHP_EOL . implode("\n", $namespaces) . PHP_EOL . $code);
+        } catch (\ReflectionException $e) { throw new \RuntimeException($e->getMessage());}
     }
     public static function tempFile(string $code): string {
         $path = sys_get_temp_dir() . "/" . md5($code) . '.php';
