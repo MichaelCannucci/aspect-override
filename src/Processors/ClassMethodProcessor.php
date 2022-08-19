fe7ed3310110a5500fab5c0eb739bc921449971e
Michael Cannucci <michaelcannucci@gmail.com>
2022-08-19 16:46:04 -0400

    Remove old regex related constants

--- src/Processors/ClassMethodProcessor.php
+++ src/Processors/ClassMethodProcessor.php
@@ -12,23 +12,6 @@ use AspectOverride\Utility\TokenUtility;
 class ClassMethodProcessor extends AbstractProcessor {
     public const NAME = 'aspect_mock_method_override';
 
-    private const BEFORE_PATTERN = '/(private|protected|public)?(\s+function\s+\S*)\(([\s\S]*?)\)((\s*:.+?\s*)?)(\s*{)/';
-
-    private const METHOD_ARGUMENTS_INDEX = 3;
-
-    private const METHOD_RETURN_TYPE = 4;
-
-    private const AFTER_PATTERN = '/(return )(.+})(;)|(return)(\s.+?)(;)/s';
-
-    private const METHOD_OVERRIDE = /** @lang InjectablePHP */
-        'if($__fn__ = \AspectOverride\Facades\Instance::getOverwriteForClass(__CLASS__, __FUNCTION__)) { %s }';
-
-    private const METHOD_ARGUMENTS_OVERRIDE = /** @lang InjectablePHP */
-        'if($_fn__args = \AspectOverride\Facades\Instance::wrapAround(__CLASS__, __FUNCTION__, %s, ...func_get_args())) { extract($_fn__args); }';
-
-    private const METHOD_AFTER_OVERRIDE = /** @lang InjectablePHP */
-        '\AspectOverride\Facades\Instance::wrapReturn(__CLASS__, __FUNCTION__, %s)';
-
     /**
      *
      * Add the injection points for the monkey-patching
