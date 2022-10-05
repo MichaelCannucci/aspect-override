c3ce33d0278f0f8c4deb6b9824d12235d0d8f282
Michael Cannucci <michaelcannucci@gmail.com>
2022-10-04 21:48:56 -0400

    update missing static methods

--- src/Facades/AspectOverride.php
+++ src/Facades/AspectOverride.php
@@ -2,16 +2,23 @@
 
 namespace AspectOverride\Facades;
 
+use AspectOverride\Core\ClassRegistry;
 use AspectOverride\Core\Configuration;
+use AspectOverride\Core\FunctionRegistry;
 use AspectOverride\Core\Instance;
+use AspectOverride\Core\StreamInterceptor;
 
 /**
+ * @method static Instance reset()
+ * @method static Instance start()
  * @method static Configuration getConfiguration()
- * @method static bool shouldProcess(string $uri)
- * @method static void dump(string $data)
+ * @method static ClassRegistry getClassRegistry()
+ * @method static FunctionRegistry getFunctionRegistry()
+ * @method static StreamInterceptor getStreamInterceptor()
  * @method static Instance resetRegistry()
  * @method static callable|null getForFunction(string $fn)
  * @method static callable|null wrapAround(string $class, string $method, array $args, callable $execute)
+ * @method static bool shouldProcess(string $path)
  */
 class AspectOverride {
     /** @var Instance|null */
@@ -39,7 +46,5 @@ class AspectOverride {
 
     public static function initialize(Configuration $configuration): void {
         self::$instance = new Instance($configuration);
-        self::$instance->reset();
-        self::$instance->start();
     }
 }
